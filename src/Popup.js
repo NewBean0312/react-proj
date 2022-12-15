ec40dd0d9eb45b53fc26d9af710bc1c409723239
NewBean0312 <newbean0312@gmail.com>
2022-12-15 20:39:39 +0900

    팝업버튼관련 로직을 두줄에서 한줄로 줄이기

--- src/Popup.js
+++ src/Popup.js
@@ -5,8 +5,7 @@ function Popup() {
   const [popupVisible, setPopupVisible] = useState(true);
   return (
     <>
-      {popupVisible && (<button className="btn btn-outline" onClick={() => setPopupVisible(false)}>팝업닫기</button>)}
-      {popupVisible || (<button className="btn btn-outline" onClick={() => setPopupVisible(true)}>팝업열기</button>)}
+      <button className="btn btn-outline" onClick={() => setPopupVisible(!popupVisible)}>팝업{popupVisible ? '닫기' : '열기'}</button>
       <hr />
       {popupVisible && <div style={{width: 100, height: 100, border}}></div>}
     </>
