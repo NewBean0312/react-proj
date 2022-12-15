c8471c254d3a97204c7728a61f548e7380868284
NewBean0312 <newbean0312@gmail.com>
2022-12-15 15:36:41 +0900

    if문을 사용해서 짝수일 때만, 문구 표시

--- src/NumberCounter.js
+++ src/NumberCounter.js
@@ -3,11 +3,22 @@ import React, { useState } from "react";
 function NumberCounter() {
   const [no, setNo] = useState(0);
 
+  const noIsEvenDiv =
+    no % 2 == 0 ? (
+      <>
+        <hr />
+        <span>짝수입니다.</span>
+      </>
+    ) : (
+      <></>
+    );
+
   return (
     <>
       숫자 : {no}
       <hr />
       <button onClick={() => setNo(no + 1)}>증가</button>
+      {noIsEvenDiv}
     </>
   )
 }
