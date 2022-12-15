9149cc86f3a8eeaa35946a4c64b8d21c55666299
NewBean0312 <newbean0312@gmail.com>
2022-12-15 20:13:26 +0900

    닫기버튼 누르면 팝업 제거

--- src/Popup.js
+++ src/Popup.js
@@ -2,11 +2,17 @@ import React, {useState} from "react"
 
 function Popup() {
   const border = '10px solid red';
+  const [popupVisible, setPopupVisible] = useState(true);
   return (
     <>
-      <button>팝업닫기</button>
+      <button 
+        className="btn btn-outline"
+        onClick={() => setPopupVisible(false)}
+      >
+        팝업닫기
+      </button>
       <hr />
-      <div style={{width: 100, height: 100, border}}></div>
+      {popupVisible && <div style={{width: 100, height: 100, border}}></div>}
     </>
   );
 }
