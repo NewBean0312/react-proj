172717a71067996fd3699925f811b85625f58202
NewBean0312 <newbean0312@gmail.com>
2022-12-15 15:10:03 +0900

    재개

--- src/StopWatch.js
+++ src/StopWatch.js
@@ -5,8 +5,10 @@ function StopWatch() {
 
   const timeoutId = setTimeout(() => setNum(num + 1), 1000);
 
-  const pause = () => {
-    clearTimeout(timeoutId);
+  const pause = () => clearTimeout(timeoutId);
+
+  const resume = () => {
+    setTimeout(() => setNum(num +1), 1000);
   };
 
   return (
@@ -14,6 +16,8 @@ function StopWatch() {
       숫자 : {num}
       <hr />
       <button onClick={pause}>일시정지</button>
+      <hr />
+      <button onClick={resume}>재개</button>
     </>
   )
 }
