77f75aacccfc246055566e617cac377ec55a0694
NewBean0312 <newbean0312@gmail.com>
2022-12-15 15:07:27 +0900

    일시정지

--- src/StopWatch.js
+++ src/StopWatch.js
@@ -3,9 +3,19 @@ import React, { useState } from "react";
 function StopWatch() {
   const [num, setNum] = useState(0);
 
-  setTimeout(() => setNum(num + 1), 1000);
+  const timeoutId = setTimeout(() => setNum(num + 1), 1000);
 
-  return <>숫자 : {num}</>;
+  const pause = () => {
+    clearTimeout(timeoutId);
+  };
+
+  return (
+    <>
+      숫자 : {num}
+      <hr />
+      <button onClick={pause}>일시정지</button>
+    </>
+  )
 }
 
 export default StopWatch;
