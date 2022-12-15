052028a73535f63d7b15993ae48a69b5992ab9ee
NewBean0312 <newbean0312@gmail.com>
2022-12-15 21:32:41 +0900

    상품이미지 및 상품명과 상품가격 추가

--- src/App.js
+++ src/App.js
@@ -5,9 +5,14 @@ import Popup from "./Popup"
 function App() {
   return (
     <>
-      {/* <StopWatch />;
-      <NumberCounter /> */}
-      <Popup />
+      {/* <StopWatch />
+      <NumberCounter />
+      <Popup /> */}
+      <div style={{display: "inline-flex", flexDirection: "column", gap: "10px"}}>
+        <img src="https://picsum.photos/id/201/400/400"></img>
+        <div style={{textAlign: "center"}}>MAC OS</div>
+        <div style={{textAlign: "center"}}>4,340,000</div>
+      </div>
     </>
   );
 }
