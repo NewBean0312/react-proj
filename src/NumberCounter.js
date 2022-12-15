56d84ca0fce9ab95ed086682d799d55ea136d56a
NewBean0312 <newbean0312@gmail.com>
2022-12-15 15:46:26 +0900

    or 연산자 사용

--- src/NumberCounter.js
+++ src/NumberCounter.js
@@ -3,20 +3,16 @@ import React, { useState } from "react";
 function NumberCounter() {
   const [no, setNo] = useState(0);
 
-  const noIsEvenDiv =
-    no % 2 == 0 ? (
-      <>
-        <hr />
-        <span>짝수입니다.</span>
-      </>
-    ) : (
-      <></>
-    );
+  const noIsEvenDiv = (
+    <>
+      <hr /> {no % 2 == 0 ? <div>짝수입니다.</div> : <div>홀수입니다.</div>}
+    </>
+  );
 
-  const noIs8MultipleDiv = no % 8 == 0 && (
+  const noIs8MultipleDiv = no % 8 == 0 || (
     <>
       <hr />
-      <span>8의 배수입니다.</span>
+      <span>8의 배수가 아닙니다.</span>
     </>
   )
 
