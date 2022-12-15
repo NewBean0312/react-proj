a53bfdf2e2961c8f0a01a0b1b682c8db7775a54d
NewBean0312 <newbean0312@gmail.com>
2022-12-15 21:38:25 +0900

    품목을 3개로 늘림

--- src/App.js
+++ src/App.js
@@ -8,10 +8,42 @@ function App() {
       {/* <StopWatch />
       <NumberCounter />
       <Popup /> */}
-      <div style={{display: "inline-flex", flexDirection: "column", gap: "10px"}}>
-        <img src="https://picsum.photos/id/201/400/400"></img>
-        <div style={{textAlign: "center"}}>MAC OS</div>
-        <div style={{textAlign: "center"}}>4,340,000</div>
+      <div style={{display:'flex', gap:'10px'}}>
+        <div
+          style={{
+            display: "inline-flex",
+              flexDirection: "column",
+                gap: "10px",
+          }}
+          >
+          <img src="https://picsum.photos/id/201/400/400" />
+          <div style={{textAlign:'center'}}>MAC BOOK AIR</div>
+          <div style={{textAlign:'center'}}>4,340,000</div>
+        </div>
+
+        <div
+          style={{
+            display: "inline-flex",
+              flexDirection: "column",
+                gap: "10px",
+          }}
+          >
+          <img src="https://picsum.photos/id/1/400/400" />
+          <div style={{textAlign:'center'}}>MAC BOOK PRO</div>
+          <div style={{textAlign:'center'}}>4,340,000</div>
+        </div>
+
+        <div
+          style={{
+            display: "inline-flex",
+              flexDirection: "column",
+                gap: "10px",
+          }}
+          >
+          <img src="https://picsum.photos/id/2/400/400" />
+          <div style={{textAlign:'center'}}>MAC BOOK PRO PLUS</div>
+          <div style={{textAlign:'center'}}>4,340,000</div>
+        </div>
       </div>
     </>
   );
