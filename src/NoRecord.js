e5d5a6df79bb40a06d8c70f64e6cacd4ec0e4839
NewBean0312 <newbean0312@gmail.com>
2022-12-16 21:11:31 +0900

    배열 렌더링

--- src/NoRecord.js
+++ src/NoRecord.js
@@ -1,6 +1,12 @@
 import React, {useState} from "react"
 
 function NoRecord() {
+  const [arr, setArr] = useState([1, 2, 3, 4, 5]);
+
+  setTimeout(() => {
+    setArr([...arr, arr.length + 1])
+  }, 1000);
+
   const [no, setNo] = useState(0);
   const [recordNo, setRecordNo] = useState(0);
   const saveNo = () => {
@@ -9,11 +15,12 @@ function NoRecord() {
 
   return (
     <>
-      <h1>숫자기록</h1>
+      {arr.join(",")}
+      {/* <h1>숫자기록</h1>
       <input type="number" value={no} onChange={(e) => setNo(e.target.valueAsNumber)} className="input w-full max-w-xs" />
       <button type="button" onClick={saveNo} className="btn btn-outline">기록</button>
       <hr />
-      기록된 숫자 : {recordNo}
+      기록된 숫자 : {recordNo} */}
     </>
   );
 }
