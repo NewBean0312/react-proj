f5d3d4e715048c142f7ce8ceb789c63a312dc958
NewBean0312 <newbean0312@gmail.com>
2022-12-16 11:34:36 +0900

    이미지번호, 상품명, 상품가격을 별도의 변수로 분리

--- src/ProductListItem.js
+++ src/ProductListItem.js
@@ -1,13 +1,17 @@
 import React, {useState} from "react"
 
 function ProductListItem() {
+  const imgNo = 1;
+  const productName = 'MAC BOOK AIR';
+  const productPriceFormatted = '4,340,000';
+
   return (
     <>
       <div
           style={{display: "inline-flex", flexDirection: "column", gap: "10px"}}>
-          <img src="https://picsum.photos/id/1/400/400" />
-          <div style={{textAlign:'center', fontWeight: 'bold', color: '#454545'}}>MAC BOOK AIR</div>
-          <div style={{textAlign:'center', color: '#454545'}}>4,340,000</div>
+          <img src={`https://picsum.photos/id/${imgNo}/400/400`} />
+          <div style={{textAlign:'center', fontWeight: 'bold', color: '#454545'}}>{productName}</div>
+          <div style={{textAlign:'center', color: '#454545'}}>{productPriceFormatted}원</div>
         </div>
     </>
   );
