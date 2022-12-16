41e86cbff87eaef4ee5f02e417d8681d3467d038
NewBean0312 <newbean0312@gmail.com>
2022-12-16 20:13:19 +0900

    반응형 상품리스트

--- src/ProdList.js
+++ src/ProdList.js
@@ -3,14 +3,23 @@ import ProdListItem from "./ProdListItem"
 function ProdList() {
   return (
     <>
-      <ul className="flex gap-[10px]">
-        <li className="flex-grow">
+      <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl: grid-cols-6 gap-[20px]">
+        <li>
           <ProdListItem imgNo={201} name='PRODUCT 1' price={'140,000'} />
         </li>
-        <li className="flex-grow">
+        <li>
           <ProdListItem imgNo={1} name='PRODUCT 2' price={'320,000'} />
         </li>
-        <li className="flex-grow">
+        <li>
+          <ProdListItem imgNo={2} name='PRODUCT 3' price={'340,000'} />  
+        </li>
+        <li>
+          <ProdListItem imgNo={201} name='PRODUCT 1' price={'140,000'} />
+        </li>
+        <li>
+          <ProdListItem imgNo={1} name='PRODUCT 2' price={'320,000'} />
+        </li>
+        <li>
           <ProdListItem imgNo={2} name='PRODUCT 3' price={'340,000'} />  
         </li>
       </ul>
