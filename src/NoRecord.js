ce01c0852d9df096dc8c83804cff830dc4e9c651
NewBean0312 <newbean0312@gmail.com>
2022-12-20 19:19:39 +0900

    엔터 입력 시, 기록

--- src/NoRecord.js
+++ src/NoRecord.js
@@ -3,7 +3,12 @@ import React, {useState} from "react"
 function NoRecord() {
   const [no, setNo] = useState("");
   const [recordNos, setRecordNos] = useState([10, 20, 30]);
-  const saveNo = () => {
+  const saveNo = (e) => {
+    e.preventDefault();
+    // prevent : 막는다, default : 기본
+    // 링크를 클릭 했을 때, 이동을 막음
+    // 폼 전송 시, 폼이 발송되는 것을 막음
+    // 마우스 휠 => 스크롤바 이동을 막음
     if (no === '') {
         alert('숫자를 입력해주세요.');
         return;
@@ -17,8 +22,10 @@ function NoRecord() {
   return (
     <>
       <h1>숫자기록</h1>
-      <input type="number" value={no} onChange={(e) => setNo(e.target.valueAsNumber)} className="input w-full max-w-xs" />
-      <button type="button" onClick={saveNo} className="btn btn-outline">기록</button>
+      <form>
+        <input type="number" value={no} onChange={(e) => setNo(e.target.valueAsNumber)} className="input w-full max-w-xs" />
+        <button type="submit" onClick={saveNo} className="btn btn-outline">기록</button>
+      </form>
       <hr />
       <h1>기록된 숫자 v1 : {recordNos.join(',')}</h1>
       <hr />
