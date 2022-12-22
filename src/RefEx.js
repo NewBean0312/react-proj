ee489ea56ce85696addad1e6246f2103f3f73a69
NewBean0312 <newbean0312@gmail.com>
2022-12-22 20:17:21 +0900

    useRef와 useState의 차이점

--- src/RefEx.js
+++ src/RefEx.js
@@ -1,60 +1,25 @@
 import React, { useState, useRef } from "react";
 
+let RefCallCount = 0;
+
 function RefEx() {
-  const noInputRef = useRef(null);
-  const [no, setNo] = useState("");
+  RefCallCount++;
+  console.log(`RefEx가 ${RefCallCount}번 실행됨!`);
 
-  const [recordedNos, setRecordedNos] = useState([10, 20, 30]);
+  const [no1, setNo1] = useState(0);
+  const [no2, setNo2] = useState(0);
+  const no3Ref = useRef(0);
 
-  const saveNo = () => {
-    if (no === "") {
-      alert("숫자를 입력해주세요.");
-      return;
-    }
-
-    setRecordedNos([...recordedNos, no]);
-    setNo("");
-    noInputRef.current.focus();
-  };
-
-  //const li = [1, 2, 3].map((el, index) => <li key={index}>{el}</li>);
-  const li = recordedNos.map((el, index) => <li key={index}>{el}</li>);
 
   return (
     <>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          saveNo();
-        }}
-      >
-        <input
-          ref={noInputRef}
-          type="number"
-          value={no}
-          onChange={(e) => setNo(e.target.valueAsNumber)}
-        />
-        <button type="submit">기록</button>
-      </form>
-
-      <hr />
-
-      <h1>기록된 숫자 v1</h1>
-      {recordedNos.join(",")}
-
-      <hr />
-
-      <h1>기록된 숫자 v2</h1>
-      <ul>{li}</ul>
-
-      <hr />
-
-      <h1>기록된 숫자 v2-2</h1>
-      <ul>
-        {recordedNos.map((el, index) => (
-          <li key={index}>{el}</li>
-        ))}
-      </ul>
+      <button onClick={() => setNo1(no1 +1)}>숫자 1 증가 : {no1}</button>
+      <br />
+      <button onClick={() => setNo2(no2 +2)}>숫자 2 증가 : {no2}</button>
+      <br />
+      <button onClick={() => no3Ref.current++}>
+        숫자 3 증가 : {no3Ref.current}
+      </button>
     </>
   );
 }
