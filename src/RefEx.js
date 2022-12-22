7a1eb68876eaaf5df5c34e7d914f20045e2f4619
NewBean0312 <newbean0312@gmail.com>
2022-12-22 20:45:55 +0900

    숫자리스트에서 특정 숫자 제거, 첫 숫자 제거, 마지막 숫자 제거 기능 추가

--- src/RefEx.js
+++ src/RefEx.js
@@ -1,11 +1,67 @@
 import React, { useState, useRef } from "react";
 
 function RefEx() {
-  const index = 3;
-  const arr = [10, 20, 30, 40, 50];
-  const newArr = arr.filter((el, _index) => _index != index);
+  const noInputRef = useRef(null);
+  const [no, setNo] = useState("");
+  const [recordedNos, setRecordedNos] = useState([5, 10, 15, 5, 20, 25, 5, 30]);
 
-  return <>{newArr.join(',')}</>;
+  const saveNo = () => {
+    if (no === "") {
+      alert("숫자를 입력해주세요.");
+      return;
+    }
+
+    setRecordedNos([...recordedNos, no]);
+    setNo("");
+  };
+  
+  const removeNo5 = () => {
+    const newRevorededNos = recordedNos.filter((el) => el != 5);
+    setRecordedNos(newRevorededNos);
+  }
+
+  const removeFirst = () => {
+    const newRevorededNos = recordedNos.filter((el, index) => index != 0);
+    setRecordedNos(newRevorededNos);
+  }
+
+  const removeLast = () => {
+    setRecordedNos(
+      recordedNos.filter((el, index) => index != recordedNos.length - 1)
+    );
+  };
+
+  return (
+    <>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          saveNo();
+        }}
+      >
+        <input
+          type="number"
+          value={no}
+          onChange={(e) => setNo(e.target.valueAsNumber)}
+        />
+        <button type="submit">기록</button>
+      </form>
+      <hr />
+
+      <h1>기록된 숫자</h1>
+      <ul>
+        {recordedNos.map((el, index) => (
+          <li key={index}>{el}</li>
+        ))}
+      </ul>
+
+      <hr />
+
+      <button onClick={removeNo5}>숫자 5 삭제</button>
+      <button onClick={removeFirst}>처음 숫자 삭제</button>
+      <button onClick={removeLast}>마지막 숫자 삭제</button>
+    </>
+  );
 }
 
 export default RefEx;
