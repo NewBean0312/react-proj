ab545cc340bc55bbc9787804b503b668d52905e4
NewBean0312 <newbean0312@gmail.com>
2022-12-22 20:32:44 +0900

    배열에서 특정요소를 제거

--- src/RefEx.js
+++ src/RefEx.js
@@ -1,27 +1,11 @@
 import React, { useState, useRef } from "react";
 
-let RefCallCount = 0;
-
 function RefEx() {
-  RefCallCount++;
-  console.log(`RefEx가 ${RefCallCount}번 실행됨!`);
+  const index = 3;
+  const arr = [10, 20, 30, 40, 50];
+  const newArr = arr.filter((el, _index) => _index != index);
 
-  const [no1, setNo1] = useState(0);
-  const [no2, setNo2] = useState(0);
-  const no3Ref = useRef(0);
-
-
-  return (
-    <>
-      <button onClick={() => setNo1(no1 +1)}>숫자 1 증가 : {no1}</button>
-      <br />
-      <button onClick={() => setNo2(no2 +2)}>숫자 2 증가 : {no2}</button>
-      <br />
-      <button onClick={() => no3Ref.current++}>
-        숫자 3 증가 : {no3Ref.current}
-      </button>
-    </>
-  );
+  return <>{newArr.join(',')}</>;
 }
 
 export default RefEx;
