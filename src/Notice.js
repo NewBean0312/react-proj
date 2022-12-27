14397ffb6e77715ddb6b488bc242580dbe78a13b
NewBean0312 <newbean0312@gmail.com>
2022-12-27 20:35:12 +0900

    NotifyOnce 컴포넌트로 Alert 컴포넌트를 감싸서 숨김

--- src/Notice.js
+++ src/Notice.js
@@ -1,3 +1,21 @@
+import classnames from "https://cdn.skypack.dev/classnames";
+
+function NotifyOnce({children}) {
+  setTimeout(function () {}, 1000);
+
+  return (
+    <div 
+      className={classnames(
+        "fixed transition-all right-[10px]",
+        {"top-[-60px]": true},
+        {"top-[10px]": false},
+      )}
+    >
+      {children}
+    </div>
+  );
+}
+
 function Alert({color:color_, children}) {
   const color = color_ ?? "red";
   return (
@@ -17,9 +35,15 @@ function Alert({color:color_, children}) {
 function Notice() {
   return (
     <>
-      <Alert>"안녕" 반가워</Alert>
-      <br />
-      <Alert color="black">지금은 수업중이야</Alert>
+      <NotifyOnce>
+        <Alert>"안녕" 반가워</Alert>
+      </NotifyOnce>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+        Reprehenderit, vitae molestiae eius quasi, dolores, eum similique ullam
+        impedit facilis maiores ipsum sunt architecto ad deleniti iste sint 
+        odio libero aut.
+      </div>
     </>
   );
 }
