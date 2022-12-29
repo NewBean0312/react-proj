2ac8c54f2d34c62b3d8cac4f2b1bb929f36cde6e
NewBean0312 <newbean0312@gmail.com>
2022-12-29 19:24:51 +0900

    의존성 배열 인자를 제거하면, 매번 실행은 되지만 타이밍이 늦어짐

--- src/App.js
+++ src/App.js
@@ -5,15 +5,20 @@ import "./App.css" ;
 let AppCallCount = 0;
 
 function App() {
+  console.log('시작');
+
   useEffect(() => {
     AppCallCount++;
     console.log(`App이 ${AppCallCount}번 실행됨!`)
-  }, []);
+  });
+  console.log('끝');
 
   const [no, SetNo] = useState(0);
-  return <>
-    <button onClick={() => SetNo(no + 1)}>증가 : {no} </button>
-  </>
+  return (
+    <>
+      <button onClick={() => SetNo(no + 1)}>증가 : {no} </button>
+    </>
+  )
 }
 
 export default App;
