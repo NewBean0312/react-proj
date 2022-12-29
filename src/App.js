553d2ae82f9e89179601231b811958d1fb800ae8
NewBean0312 <newbean0312@gmail.com>
2022-12-29 21:14:27 +0900

    다크모드 구현

--- src/App.js
+++ src/App.js
@@ -2,60 +2,37 @@ import React, { useState, useEffect, useRef } from "react";
 
 import "./App.css";
 
-let AppCallCount = 0;
-let SubCallCount = 0;
-
-function Sub({ appNo }) {
-  SubCallCount++;
-  console.log(`Sub가 ${SubCallCount}번 실행됨!`);
-
-  const [no, setNo] = useState(0);
-  const [no2, setNo2] = useState(0);
-
-  useEffect(() => {
-    console.log('effect 1 : 단 한 번 실행');
-  }, []);
-
-  useEffect(() => {
-    console.log('effect 2 : 부모(App)의 appNo가 바뀔 때 마다 실행');
-  }, [appNo]);
-
-  useEffect(() => {
-    console.log('effect 3 : 나(Sub)의 no가 바뀔 때 마다 실행');
-  }, [no]);
-
-  useEffect(() => {
-    console.log('effect 4 : appNo 혹은 no가 변경될 때 마다 실행');
-  }, [appNo, no]);
-
-  useEffect(() => {
-    console.log('effect 5 : 매 번 실행');
-  });
-
-  return (
-    <>
-      <div style={{ border: "10px solid blue", padding: 10 }}>
-        App no : {appNo}
-        <hr />
-        <button onClick={() => setNo(no + 1)}>버튼 : {no}</button>
-        <button onClick={() => setNo2(no2 + 1)}>버튼2 : {no2}</button>
-      </div>
-    </>
-  );
-}
-
 function App() {
-  AppCallCount++;
-  console.log(`App이 ${AppCallCount}번 실행됨!`);
   const [no, setNo] = useState(0);
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    const html = document.getElementsByTagName("html")[0];
+
+    if (isDark) {
+      html.classList.add("dark");
+    } else {
+      html.classList.remove("dark");
+    }
+  }, [isDark]);
 
   return (
     <>
-      <div style={{ border: "10px solid red", padding: 10 }}>
-        <button onClick={() => setNo(no + 1)}>버튼 : {no}</button>
-        <hr />
-        <Sub appNo={no}/>
+      <div>
+        <button className="btn-toggle-theme" onClick={() => setIsDark(!isDark)}>
+          테마토글
+        </button>
+        <button onClick={() => setNo(no + 1)}>숫자 증가 : {no}</button>
       </div>
+
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis
+        tempore similique quaerat, rerum sunt alias repellat aliquid! Nesciunt
+        fugit maiores quia obcaecati sed! A veniam eos earum porro eaque
+        commodi?
+      </div>
+
+      <h1 className="color-primary">하하 호호</h1>
     </>
   );
 }
