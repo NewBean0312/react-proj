8d0ace82893ebbb6d7e86d4374c080c56663e3b0
NewBean0312 <newbean0312@gmail.com>
2023-01-13 20:32:24 +0900

    음식 옵션 리스트 아이템 체크 기능

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
-import React, { useState, useCallback } from "react";
-import Order from "./Order";
+import React, { useState } from "react";
 
 import "./App.css";
 
-
 function App() {
+  const fruits = ["사과", "배", "바나나"];
+
+  const [selecteds, setSelecteds] = useState(
+    new Array(fruits.length).fill(true)
+  );
+
+  const toggleFruitSelected = (index) => {
+    const newSelecteds = selecteds.map((el, _index) =>
+      _index == index ? !el : el
+    );
+    setSelecteds(newSelecteds);
+  };
+
+  const selectedFruits = selecteds
+    .map((el, index) => (el ? fruits[index] : el))
+    .filter((el) => el);
 
   return (
     <>
-      <Order />
+      <ul>
+        {fruits.map((fruit, index) => (
+          <li key={index}>
+            <label>
+              <input
+                checked={selecteds[index]}
+                type="checkbox"
+                onChange={() => toggleFruitSelected(index)}
+              />
+              {fruit}
+            </label>
+          </li>
+        ))}
+      </ul>
+      <hr />
+      <div>
+        선택상태 : {selecteds.join(", ")}
+        <hr />
+        선택된 과일 : {selectedFruits.join(", ")}
+      </div>
     </>
   );
 }
