56aeed183961e4c58304aa451fcb744cdd45bfda
NewBean0312 <newbean0312@gmail.com>
2023-01-17 15:58:10 +0900

    할일수정 함수를 만들면 그 이후가 편함

--- src/App.js
+++ src/App.js
@@ -6,12 +6,19 @@ function App() {
   const addTodo = (newTodo) => {
     const newTodos = [...todos, newTodo];
     setTodos(newTodos);
-  }
+  };
 
   const removeTodo = (index) => {
     const newTodos = todos.filter((_, _index) => _index != index);
     setTodos(newTodos);
-  }
+  };
+
+  const modifyTodo = (index, newTodo) => {
+    const newTodos = todos.map((todo, _index) =>
+      _index != index ? todo : newTodo
+    );
+    setTodos(newTodos);
+  };
 
   const onAddBtnClick = () => {
     addTodo(todos.length + 1);
@@ -21,11 +28,16 @@ function App() {
     removeTodo(1);
   };
 
+  const onEditBtnClick = () => {
+    modifyTodo(1, "안녕");
+  };
+
   return (
     <>
       <div>{JSON.stringify(todos)}</div>
       <button onClick={onAddBtnClick}>추가</button>
       <button onClick={onRemoveBtnClick}>삭제</button>
+      <button onClick={onEditBtnClick}>수정</button>
     </>
   );
 }
