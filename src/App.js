6afcdb28a730014d3f8a1cff31ad410ddc25dd8f
NewBean0312 <newbean0312@gmail.com>
2023-01-17 19:46:51 +0900

    할 일 삭제

--- src/App.js
+++ src/App.js
@@ -19,13 +19,23 @@ function App() {
     setTodos(newTodos);
   };
 
+  const removeTodo = (index) => {
+    const newTodos = todos.filter((_, _index) => _index != index);
+    setTodos(newTodos);
+  };
+
   const onBtnAddTodoClick = () => {
     addTodo("안녕");
+  };
+
+  const onBtnDeleteTodoClick = () => {
+    removeTodo(1);
   };
 
   return (
     <>
       <button onClick={onBtnAddTodoClick}>추가</button>
+      <button onClick={onBtnDeleteTodoClick}>삭제</button>
       <hr />
       <ul>
         {todos.map((todo, index) => (
