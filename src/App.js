309302b721e1a1bf8ee96400c43c63746cc8ea20
NewBean0312 <newbean0312@gmail.com>
2023-01-17 19:21:30 +0900

    객체형식의 할일을 추가하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,43 +1,36 @@
 import React, { useState } from "react";
 
+import "./App.css";
+
 function App() {
   const [todos, setTodos] = useState([]);
 
-  const addTodo = (newTodo) => {
+  const addTodo = (newContent) => {
+    const newTodo = {
+      id: 1,
+      content: newContent,
+      regDate: "2023-01-17 12:12:12",
+    };
+
     const newTodos = [...todos, newTodo];
     setTodos(newTodos);
   };
 
-  const removeTodo = (index) => {
-    const newTodos = todos.filter((_, _index) => _index != index);
-    setTodos(newTodos);
-  };
-
-  const modifyTodo = (index, newTodo) => {
-    const newTodos = todos.map((todo, _index) =>
-      _index != index ? todo : newTodo
-    );
-    setTodos(newTodos);
-  };
-
-  const onAddBtnClick = () => {
-    addTodo(todos.length + 1);
-  };
-
-  const onRemoveBtnClick = () => {
-    removeTodo(1);
-  };
-
-  const onEditBtnClick = () => {
-    modifyTodo(1, "안녕");
+  const onBtnAddTodoClick = () => {
+    addTodo("안녕");
   };
 
   return (
     <>
-      <div>{JSON.stringify(todos)}</div>
-      <button onClick={onAddBtnClick}>추가</button>
-      <button onClick={onRemoveBtnClick}>삭제</button>
-      <button onClick={onEditBtnClick}>수정</button>
+      <button onClick={onBtnAddTodoClick}>추가</button>
+      <hr />
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo.id} {todo.content} {todo.regDate}
+          </li>
+        ))}
+      </ul>
     </>
   );
 }
