07aab6644469d2f0648f9567e83c359938f3b4fe
NewBean0312 <newbean0312@gmail.com>
2023-01-20 17:58:23 +0900

    할일 추가 폼

--- src/App.js
+++ src/App.js
@@ -1,20 +1,110 @@
 import React, { useState, useRef } from "react";
 import { AppBar, Toolbar } from "@mui/material";
 
-import "./App.css";
+function useTodosState() {
+  const [todos, setTodos] = useState([]);
+  const lastTodoIdRef = useRef(0);
+
+  const addTodo = (newContent) => {
+    const id = ++lastTodoIdRef.current;
+
+    const newTodo = {
+      id,
+      content: newContent,
+      regDate: dateToStr(new Date()),
+    };
+
+    const newTodos = [...todos, newTodo];
+    setTodos(newTodos);
+  };
+
+  const modifyTodo = (index, newContent) => {
+    const newTodos = todos.map((todo, _index) =>
+      _index != index ? todo : { ...todo, content: newContent }
+    );
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (index) => {
+    const newTodos = todos.filter((_, _index) => _index != index);
+    setTodos(newTodos);
+  };
+
+  return {
+    todos,
+    addTodo,
+    removeTodo,
+    modifyTodo,
+  };
+}
 
 function App() {
+  const todosState = useTodosState();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const form = e.target;
+
+    form.content.value = form.content.value.trim();
+
+    if (form.content.value.length == 0) {
+      alert("할일을 입력해주세요.");
+      form.content.focus();
+
+      return;
+    }
+
+    todosState.addTodo(form.content.value);
+    form.content.value = "";
+    form.content.focus();
+  };
+
   return (
     <>
-      <AppBar position="fixed">
-        <Toolbar>
+      <AppBar position="static">
+        <Toolbar className="justify-center">
           <div className="flex-1"></div>
-          <div className="flex-bold">NOTEPAD</div>
+          <div className="flex-bold">HAPPY NOTE</div>
           <div className="flex-1"></div>
         </Toolbar>
       </AppBar>
+      <form onSubmit={onSubmit}>
+        <input
+          autoComplete="off"
+          name="content"
+          type="text"
+          placeholder="할일을 입력해주세요."
+        />
+        <input type="submit" value="추가" />
+        <input type="reset" value="취소" />
+      </form>
+      {todosState.todos.length}
     </>
   );
 }
 
 export default App;
+
+// 유틸리티
+
+// 날짜 객체 입력받아서 문장(yyyy-mm-dd hh:mm:ss)으로 반환한다.
+function dateToStr(d) {
+  const pad = (n) => {
+    return n < 10 ? "0" + n : n;
+  };
+
+  return (
+    d.getFullYear() +
+    "-" +
+    pad(d.getMonth() + 1) +
+    "-" +
+    pad(d.getDate()) +
+    " " +
+    pad(d.getHours()) +
+    ":" +
+    pad(d.getMinutes()) +
+    ":" +
+    pad(d.getSeconds())
+  );
+}
