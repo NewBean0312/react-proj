31e736ca4359972d0abb96410c6a2537568adc8d
NewBean0312 <newbean0312@gmail.com>
2023-01-30 18:54:21 +0900

    수정을 위한 modal

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
   List,
   ListItem,
   ListItemButton,
+  Modal,
 } from "@mui/material";
 import classNames from "classnames";
 
@@ -213,6 +214,13 @@ function TodoOptionDrawer({ todosState, state }) {
           </ListItemButton>
         </List>
       </SwipeableDrawer>
+      <Modal
+        open={true}
+        onClose={() => {}}
+        className="flex justify-center items-center"
+      >
+        <div className="bg-white p-10 rounded-[20px]">안녕하세요</div>
+      </Modal>
     </>
   );
 }
