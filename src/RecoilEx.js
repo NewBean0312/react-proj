8ac3ec2943b87fdd60ff9c69d6cb42788bdd5bca
NewBean0312 <newbean0312@gmail.com>
2023-02-01 12:39:33 +0900

    리코일이 필요한 상황 만들기 (각 페이지에서 no 값을 유지해야 하는 상황)

--- src/RecoilEx.js
+++ src/RecoilEx.js
@@ -2,24 +2,44 @@ import React, { useState } from "react";
 import { Button } from "@mui/material";
 
 function Page1() {
+  const [no, setNo] = useState(0);
   return (
     <>
       <h1>페이지 1</h1>
       <ul>
-        <li>페이지 1 내용 1</li>
-        <li>페이지 1 내용 2</li>
+        <li>페이지 1의 숫자 : {no}</li>
+        <li>
+          <Button onClick={() => setNo(no + 10)} variant="outlined">
+            페이지 1의 10 증가
+          </Button>
+        </li>
+        <li>
+          <Button onClick={() => setNo(no - 10)} variant="outlined">
+            페이지 1의 10 감소
+          </Button>
+        </li>
       </ul>
     </>
   );
 }
 
 function Page2() {
+  const [no, setNo] = useState(0);
   return (
     <>
       <h1>페이지 2</h1>
       <ul>
-        <li>페이지 2 내용 1</li>
-        <li>페이지 2 내용 2</li>
+        <li>페이지 2의 숫자 : {no}</li>
+        <li>
+          <Button onClick={() => setNo(no + 10)} variant="outlined">
+            페이지 2의 10 증가
+          </Button>
+        </li>
+        <li>
+          <Button onClick={() => setNo(no - 10)} variant="outlined">
+            페이지 2의 10 감소
+          </Button>
+        </li>
       </ul>
     </>
   );
