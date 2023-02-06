e30354625540b451aa130b580a5bbe249ae79cc9
NewBean0312 <newbean0312@gmail.com>
2023-02-06 12:22:24 +0900

    이전 페이지로 이동, 다음 페이지로 이동

--- src/RouterEx.js
+++ src/RouterEx.js
@@ -27,6 +27,7 @@ function LoginPage() {
 
 function useHistory() {
   const initialUrl = "home";
+  const [currentIndex, setCurrentIndex] = useState(0);
   const [url, setUrl] = useState("home");
   const [historyUrls, setHistoryUrls] = useState([initialUrl]);
 
@@ -35,21 +36,51 @@ function useHistory() {
     setHistoryUrls([url, ...historyUrls]);
   };
 
+  const movePrev = () => {
+    if (currentIndex == historyUrls.length - 1) {
+      return false;
+    }
+
+    const url = historyUrls[currentIndex + 1];
+    setUrl(url);
+    setCurrentIndex(currentIndex + 1);
+  };
+
+  const moveNext = () => {
+    if (currentIndex == 0) {
+      return false;
+    }
+
+    const url = historyUrls[currentIndex - 1];
+    setUrl(url);
+    setCurrentIndex(currentIndex - 1);
+  };
+
   return {
     url,
     movePage,
     historyUrls,
-  }
+    movePrev,
+    moveNext,
+  };
 }
 
 export default function RouterEx() {
   const history = useHistory();
 
-
   return (
     <>
       <div className="p-5">현재 주소 : {history.url}</div>
-      <div className="p-5">히스토리 : {history.historyUrls.join(",")}</div>
+      <div className="p-5">
+        히스토리 : {history.historyUrls.join(",")}
+        <br />
+        <Button variant="outlined" onClick={history.movePrev}>
+          뒤로가기
+        </Button>
+        <Button variant="outlined" onClick={history.moveNext}>
+          앞으로가기
+        </Button>
+      </div>
       <ul className="flex gap-3 p-5">
         <li
           onClick={() => history.movePage("home")}
