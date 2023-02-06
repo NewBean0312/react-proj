6aa3f56e5cd49c3e5386b6d1c51071badda8ec64
NewBean0312 <newbean0312@gmail.com>
2023-02-06 12:08:25 +0900

    페이지 이동기록 히스토리

--- src/RouterEx.js
+++ src/RouterEx.js
@@ -25,36 +25,55 @@ function LoginPage() {
   );
 }
 
-export default function RouterEx() {
+function useHistory() {
+  const initialUrl = "home";
   const [url, setUrl] = useState("home");
+  const [historyUrls, setHistoryUrls] = useState([initialUrl]);
+
+  const movePage = (url) => {
+    setUrl(url);
+    setHistoryUrls([url, ...historyUrls]);
+  };
+
+  return {
+    url,
+    movePage,
+    historyUrls,
+  }
+}
+
+export default function RouterEx() {
+  const history = useHistory();
+
 
   return (
     <>
-      <div className="p-5">현재 주소 : {url}</div>
+      <div className="p-5">현재 주소 : {history.url}</div>
+      <div className="p-5">히스토리 : {history.historyUrls.join(",")}</div>
       <ul className="flex gap-3 p-5">
         <li
-          onClick={() => setUrl("home")}
+          onClick={() => history.movePage("home")}
           className="hover:text-red-300 cursor-pointer"
         >
           <Button variant="contained">Home</Button>
         </li>
         <li
-          onClick={() => setUrl("about")}
+          onClick={() => history.movePage("about")}
           className="hover:text-red-300 cursor-pointer"
         >
           <Button variant="contained">About</Button>
         </li>
         <li
-          onClick={() => setUrl("login")}
+          onClick={() => history.movePage("login")}
           className="hover:text-red-300 cursor-pointer"
         >
           <Button variant="contained">Login</Button>
         </li>
       </ul>
       <div className="p-5">
-        {url == "home" && <HomePage />}
-        {url == "about" && <AboutPage />}
-        {url == "login" && <LoginPage />}
+        {history.url == "home" && <HomePage />}
+        {history.url == "about" && <AboutPage />}
+        {history.url == "login" && <LoginPage />}
       </div>
     </>
   );
