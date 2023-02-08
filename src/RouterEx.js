69a431b05528acb07dc46391ba14a14ea8476233
NewBean0312 <newbean0312@gmail.com>
2023-02-08 11:45:26 +0900

    useParams를 이용해서 url 파라미터 얻기

--- src/RouterEx.js
+++ src/RouterEx.js
@@ -5,6 +5,7 @@ import {
   Navigate,
   useLocation,
   NavLink,
+  useParams,
 } from "react-router-dom";
 import classNames from "classnames";
 
@@ -20,6 +21,35 @@ function HomeAboutPage() {
   return (
     <>
       <h1>HOME, ABOUT</h1>
+    </>
+  );
+}
+
+function ArticleListPage() {
+  const articles = [{ id: 1 }, { id: 2 }];
+  return (
+    <>
+      <h1>ARTICLE LIST</h1>
+      <ul>
+        {articles.map((article) => (
+          <li key={article.id}>
+            <NavLink to={`/article/detail/${article.id}`}>
+              {article.id}번 게시물
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+function ArticleDetailPage() {
+  const { id } = useParams();
+
+  return (
+    <>
+      <h1>ARTICLE, DETAIL</h1>
+      <h1>{id}번 게시물 상세페이지</h1>
     </>
   );
 }
@@ -55,9 +85,23 @@ export default function ReouterEx() {
       >
         ABOUT
       </NavLink>
+      <NavLink
+        to="/article/list"
+        className={({ isActive }) =>
+          classNames(
+            "btn",
+            { "btn-link": !isActive },
+            { "btn-primary": isActive }
+          )
+        }
+      >
+        ARTICLE LIST
+      </NavLink>
       <Routes>
         <Route path="/home/main" element={<HomeMainPage />} />
         <Route path="/home/about" element={<HomeAboutPage />} />
+        <Route path="/article/list" element={<ArticleListPage />} />
+        <Route path="/article/detail/:id" element={<ArticleDetailPage />} />
         <Route path="*" element={<Navigate to="/home/main" />} />
       </Routes>
     </>
