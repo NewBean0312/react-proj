252ff0e24876c3422570782a06fd0653d76af439
NewBean0312 <newbean0312@gmail.com>
2023-02-09 14:22:13 +0900

    immer를 할 일 추가에 적용

--- src/RouterEx.js
+++ src/RouterEx.js
@@ -34,7 +34,9 @@ function useTodosStatus() {
       content,
     };
 
-    const newTodos = [newTodo, ...todos];
+    const newTodos = produce(todos, (draft) => {
+      draft.unshift(newTodo);
+    });
     setTodos(newTodos);
   };
 
